6da8a1c8169608b56ac1b0e8785104eccf27a989
ADMIN <ADMIN@PC218>
2024-06-06 10:10:30 +0700

    bo sung

---
+++ WbeBanHang/Controllers/Productcontroller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WbeBanHang.Models;
+
+namespace WbeBanHang.Controllers
+{
+    public class Productcontroller : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hosting;
+        public Productcontroller(ApplicationDbContext db, IHostingEnvironment hosting)
+        {
+            _db = db;
+            _hosting = hosting;
+        }
+        public IActionResult Index()
+        {
+            var dsSanPham = _db.Products.ToList();
+            return View();
+        }
+    }
+}
